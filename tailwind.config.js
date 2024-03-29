4a12b97a6d872370e177f0aa3bc788efe0ad1adf
olegbobokhidze <oleg.bobokhidze.1@iliauni.edu.ge>
2024-03-29 20:11:18 +0400

    feat: add screens in tailwind config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      screens: {
+        mobile: '360px',
+        tablet: '1024px',
+        small: '1240px',
+        large: '1920px'
+      }
+    }
   },
   plugins: []
 }
