6fb9fbe2eb95ca18406ff70c0a3d52e10b4f3a72
olegbobokhidze <oleg.bobokhidze.1@iliauni.edu.ge>
2024-03-29 14:36:57 +0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>
+    <div class="wrapper"></div>
+  </header>
+
+  <main class="flex w-11 items-center justify-center">Lets Start</main>
+  <div class="flex h-11 w-10 items-center justify-center p-5"></div>
+</template>
