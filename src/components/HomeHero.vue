c66acdfb675d6e718f4da89668d577ebc70676d7
olegbobokhidze <oleg.bobokhidze.1@iliauni.edu.ge>
2024-03-29 22:57:09 +0400

    wip: add home hero

--- src/components/HomeHero.vue
+++ src/components/HomeHero.vue
@@ -1,7 +1,33 @@
+<script setup lang="ts"></script>
 <template>
-  <div>
-    <h1>Hero</h1>
+  <div class="flex h-full w-full flex-col gap-4 rounded-[1.25rem] bg-white px-4 pb-6 pt-3">
+    <div class="h-full w-full">
+      <img
+        src="@/assets/images/home-hero-image.jpeg"
+        alt="home-hero-image"
+        class="max-h-[25.375rem] w-full rounded-[1.25rem]"
+      />
+    </div>
+    <div class="flex flex-col gap-4">
+      <div class="flex gap-1">
+        <div
+          class="flex items-center justify-center rounded-2xl bg-[#F5F8FF] px-2 py-[0.375rem] text-[0.75rem] text-[#1D2D35]"
+        >
+          <p>12.08.2023</p>
+        </div>
+        <div
+          class="flex items-center justify-center rounded-2xl bg-[#F5F8FF] px-2 py-[0.375rem] text-[0.75rem] text-[#1D2D35]"
+        >
+          <p>პოლიტიკა</p>
+        </div>
+      </div>
+      <h1 class="text-xl font-semibold text-[#1D2D35] tablet:text-2xl">
+        ქვემო ქართლის რეგიონში 20 მლნ ლარამდე ღირებულების ინფრასტრუქტურული პროექტები დაიწყება
+      </h1>
+      <p class="hidden text-base text-[#657178] tablet:flex">
+        განთავსების (ლისტინგის) ორგანიზატორი სომხეთის ბანკი სს “არდშიბანკია”. ობლიგაციის ნომინალი -
+        1000 USD
+      </p>
+    </div>
   </div>
 </template>
-
-<script setup lang="ts"></script>
