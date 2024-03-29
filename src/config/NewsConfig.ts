0a46b39ce5f6afdeeed6d15252166aed62f2b0bd
olegbobokhidze <oleg.bobokhidze.1@iliauni.edu.ge>
2024-03-29 19:08:11 +0400

    wip: add news config with category all and popular and add image for news popular category.

---
+++ src/config/NewsConfig.ts
@@ -0,0 +1,70 @@
+export type INewsConfig = {
+  id: number
+  description: string
+  time: string
+  category?: string
+  image?: string
+}
+
+export const NewsConfig: INewsConfig[] = [
+  {
+    id: 0,
+    description: 'დღეიდან ასაწონად განკუთვნილი პოლიეთილენის პარკები აიკრძალა',
+    time: '7:30 02.01.2023'
+  },
+  {
+    id: 1,
+    description: 'დღეიდან ასაწონად განკუთვნილი პოლიეთილენის პარკები აიკრძალა',
+    time: '7:30 02.01.2023'
+  },
+  {
+    id: 2,
+    description: 'დღეიდან ასაწონად განკუთვნილი პოლიეთილენის პარკები აიკრძალა',
+    time: '7:30 02.01.2023'
+  },
+  {
+    id: 3,
+    description: 'დღეიდან ასაწონად განკუთვნილი პოლიეთილენის პარკები აიკრძალა',
+    time: '7:30 02.01.2023'
+  },
+  {
+    id: 4,
+    description: 'დღეიდან ასაწონად განკუთვნილი პოლიეთილენის პარკები აიკრძალა',
+    time: '7:30 02.01.2023'
+  },
+  {
+    id: 5,
+    description: 'დღეიდან ასაწონად განკუთვნილი პოლიეთილენის პარკები აიკრძალა',
+    category: 'popular',
+    image: './assets/images/home-news-popular-image.png',
+    time: '7:30 02.01.2023'
+  },
+  {
+    id: 6,
+    description: 'დღეიდან ასაწონად განკუთვნილი პოლიეთილენის პარკები აიკრძალა',
+    category: 'popular',
+    image: './assets/images/home-news-popular-image.png',
+    time: '7:30 02.01.2023'
+  },
+  {
+    id: 7,
+    description: 'დღეიდან ასაწონად განკუთვნილი პოლიეთილენის პარკები აიკრძალა',
+    category: 'popular',
+    image: './assets/images/home-news-popular-image.png',
+    time: '7:30 02.01.2023'
+  },
+  {
+    id: 8,
+    description: 'დღეიდან ასაწონად განკუთვნილი პოლიეთილენის პარკები აიკრძალა',
+    category: 'popular',
+    image: './assets/images/home-news-popular-image.png',
+    time: '7:30 02.01.2023'
+  },
+  {
+    id: 9,
+    description: 'დღეიდან ასაწონად განკუთვნილი პოლიეთილენის პარკები აიკრძალა',
+    category: 'popular',
+    image: './assets/images/home-news-popular-image.png',
+    time: '7:30 02.01.2023'
+  }
+]
