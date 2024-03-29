85158100420d090a9c7c4b95a1b30d6c057acdfd
olegbobokhidze <oleg.bobokhidze.1@iliauni.edu.ge>
2024-03-29 22:56:00 +0400

    wip: add home all news component

---
+++ src/components/HomeAllNews.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { NewsConfig } from '@/config/NewsConfig'
+import IconDate from '@/components/icons/IconDate.vue'
+</script>
+
+<template>
+  <div
+    class="flex h-[20.625rem] w-full flex-col rounded-[1.25rem] bg-white px-4 pb-6 pt-3 small:h-full small:w-[16.5rem]"
+  >
+    <h1 class="mb-5 text-base font-semibold text-[#1D2D35] tablet:text-xl">ყველა სიახლე</h1>
+    <div class="news-scrollbar flex w-full flex-col overflow-y-scroll">
+      <div class="flex h-full w-full flex-col gap-4 py-2" v-for="item in NewsConfig" :key="item.id">
+        <div class="flex items-center gap-2">
+          <IconDate />
+          <p class="text-[0.75rem] font-bold text-[#585EE3]">{{ item.time }}</p>
+        </div>
+        <p class="text-sm text-[#1D2D35]">{{ item.description }}</p>
+        <hr />
+      </div>
+    </div>
+  </div>
+</template>
