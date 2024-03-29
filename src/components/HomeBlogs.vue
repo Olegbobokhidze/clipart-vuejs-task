df6474921f9172fced92d8748307443e849e08a0
olegbobokhidze <oleg.bobokhidze.1@iliauni.edu.ge>
2024-03-30 02:02:25 +0400

    feat: add home blogs

---
+++ src/components/HomeBlogs.vue
@@ -0,0 +1,136 @@
+<template>
+  <div
+    class="grid h-full w-full grid-cols-1 gap-4 tablet:grid-cols-2 small:grid-cols-3 small:gap-x-6 small:gap-y-2"
+  >
+    <div
+      class="flex h-full w-full flex-col gap-4 rounded-[1.25rem] bg-white px-3 pb-4 pt-2 tablet:col-span-2 small:col-span-1 small:row-span-3"
+    >
+      <div class="max-h-[17.5rem] w-full small:h-[15rem]">
+        <img
+          src="@/assets/images/home-famous-facts-image.png"
+          class="h-full w-full rounded-[1.25rem]"
+          alt="famimg"
+        />
+      </div>
+      <div class="flex gap-1">
+        <div
+          class="flex items-center justify-center rounded-2xl bg-[#F5F8FF] px-2 py-[0.375rem] text-[0.75rem] text-[#1D2D35]"
+        >
+          <p>12.08.2023</p>
+        </div>
+        <div
+          class="flex items-center justify-center rounded-2xl bg-[#F5F8FF] px-2 py-[0.375rem] text-[0.75rem] text-[#1D2D35]"
+        >
+          <p>პოლიტიკა</p>
+        </div>
+      </div>
+      <h1 class="text-base text-[#1D2D35]">
+        ქვემო ქართლის რეგიონში 20 მლნ ლარამდე ღირებულების ინფრასტრუქტურული პროექტები დაიწყება
+      </h1>
+    </div>
+    <div
+      class="flex h-full w-full flex-col gap-4 rounded-[1.25rem] bg-white px-3 pb-4 pt-2 small:col-start-1 small:row-start-1"
+    >
+      <div class="max-h-[10rem] w-full small:h-[6rem]">
+        <img
+          src="@/assets/images/home-famous-facts-image.png"
+          class="h-full w-full rounded-[1.25rem]"
+          alt="famimg"
+        />
+      </div>
+      <div class="flex gap-1">
+        <div
+          class="flex items-center justify-center rounded-2xl bg-[#F5F8FF] px-2 py-[0.375rem] text-[0.75rem] text-[#1D2D35]"
+        >
+          <p>12.08.2023</p>
+        </div>
+        <div
+          class="flex items-center justify-center rounded-2xl bg-[#F5F8FF] px-2 py-[0.375rem] text-[0.75rem] text-[#1D2D35]"
+        >
+          <p>პოლიტიკა</p>
+        </div>
+      </div>
+      <h1 class="line-clamp-1 text-base text-[#1D2D35]">
+        ქვემო ქართლის რეგიონში 20 მლნ ლარამდე ღირებულების ინფრასტრუქტურული პროექტები დაიწყება
+      </h1>
+    </div>
+    <div
+      class="flex h-full w-full flex-col gap-4 rounded-[1.25rem] bg-white px-3 pb-4 pt-2 small:col-start-3 small:row-start-1"
+    >
+      <div class="max-h-[10rem] w-full small:h-[6rem]">
+        <img
+          src="@/assets/images/home-famous-facts-image.png"
+          class="h-full w-full rounded-[1.25rem]"
+          alt="famimg"
+        />
+      </div>
+      <div class="flex gap-1">
+        <div
+          class="flex items-center justify-center rounded-2xl bg-[#F5F8FF] px-2 py-[0.375rem] text-[0.75rem] text-[#1D2D35]"
+        >
+          <p>12.08.2023</p>
+        </div>
+        <div
+          class="flex items-center justify-center rounded-2xl bg-[#F5F8FF] px-2 py-[0.375rem] text-[0.75rem] text-[#1D2D35]"
+        >
+          <p>პოლიტიკა</p>
+        </div>
+      </div>
+      <h1 class="line-clamp-1 text-base text-[#1D2D35]">
+        ქვემო ქართლის რეგიონში 20 მლნ ლარამდე ღირებულების ინფრასტრუქტურული პროექტები დაიწყება
+      </h1>
+    </div>
+    <div
+      class="flex h-full w-full flex-col gap-4 rounded-[1.25rem] bg-white px-3 pb-4 pt-2 small:col-start-3 small:row-start-3"
+    >
+      <div class="max-h-[10rem] w-full small:h-[6rem]">
+        <img
+          src="@/assets/images/home-famous-facts-image.png"
+          class="h-full w-full rounded-[1.25rem]"
+          alt="famimg"
+        />
+      </div>
+      <div class="flex gap-1">
+        <div
+          class="flex items-center justify-center rounded-2xl bg-[#F5F8FF] px-2 py-[0.375rem] text-[0.75rem] text-[#1D2D35]"
+        >
+          <p>12.08.2023</p>
+        </div>
+        <div
+          class="flex items-center justify-center rounded-2xl bg-[#F5F8FF] px-2 py-[0.375rem] text-[0.75rem] text-[#1D2D35]"
+        >
+          <p>პოლიტიკა</p>
+        </div>
+      </div>
+      <h1 class="line-clamp-1 text-base text-[#1D2D35]">
+        ქვემო ქართლის რეგიონში 20 მლნ ლარამდე ღირებულების ინფრასტრუქტურული პროექტები დაიწყება
+      </h1>
+    </div>
+    <div
+      class="flex h-full w-full flex-col gap-4 rounded-[1.25rem] bg-white px-3 pb-4 pt-2 small:col-start-1 small:row-start-3"
+    >
+      <div class="max-h-[10rem] w-full small:h-[6rem]">
+        <img
+          src="@/assets/images/home-famous-facts-image.png"
+          class="h-full w-full rounded-[1.25rem]"
+          alt="famimg"
+        />
+      </div>
+      <div class="flex gap-1">
+        <div
+          class="flex items-center justify-center rounded-2xl bg-[#F5F8FF] px-2 py-[0.375rem] text-[0.75rem] text-[#1D2D35]"
+        >
+          <p>12.08.2023</p>
+        </div>
+        <div
+          class="flex items-center justify-center rounded-2xl bg-[#F5F8FF] px-2 py-[0.375rem] text-[0.75rem] text-[#1D2D35]"
+        >
+          <p>პოლიტიკა</p>
+        </div>
+      </div>
+      <h1 class="line-clamp-1 text-base text-[#1D2D35]">
+        ქვემო ქართლის რეგიონში 20 მლნ ლარამდე ღირებულების ინფრასტრუქტურული პროექტები დაიწყება
+      </h1>
+    </div>
+  </div>
+</template>
