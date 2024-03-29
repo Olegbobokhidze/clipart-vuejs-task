cde4ec5f6f852bc23c1b3d4b48bacfd7029f0825
olegbobokhidze <oleg.bobokhidze.1@iliauni.edu.ge>
2024-03-29 22:56:12 +0400

    wip: add home popular news component

---
+++ src/components/HomePopularNews.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { NewsConfig } from '@/config/NewsConfig'
+
+const PopularNews = NewsConfig.filter((item) => item.category === 'popular')
+</script>
+
+<template>
+  <div
+    class="flex h-[20.625rem] w-full flex-col rounded-[1.25rem] bg-white px-4 pb-6 pt-3 small:h-full small:w-[16.5rem]"
+  >
+    <h1 class="mb-5 text-base font-semibold text-[#1D2D35] tablet:text-xl">პოპულარული</h1>
+    <div class="news-scrollbar flex w-full flex-col overflow-y-scroll">
+      <div
+        class="flex h-full w-full flex-row items-center gap-2 py-2"
+        v-for="item in PopularNews"
+        :key="item.id"
+      >
+        <div class="h-[96px] min-w-[96px]">
+          <img class="h-full w-full" :src="item.image" />
+        </div>
+        <p class="line-clamp-5 text-[0.75rem] text-[#1D2D35] mobile:text-sm">
+          {{ item.description }}
+        </p>
+        <hr />
+      </div>
+    </div>
+  </div>
+</template>
