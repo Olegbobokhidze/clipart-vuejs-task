5572613f0634a5d2dcbaa88a22dd6967b41eac79
olegbobokhidze <oleg.bobokhidze.1@iliauni.edu.ge>
2024-03-29 22:58:08 +0400

    wip: add home layout in app

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import HomeHero from '@/components/HomeHero.vue'
+import HomeAllNews from './components/HomeAllNews.vue'
+import HomePopularNews from './components/HomePopularNews.vue'
+import { useWindowSize } from '@vueuse/core'
+
+const { width } = useWindowSize()
+</script>
 
 <template>
-  <header>
-    <div class="wrapper"></div>
-  </header>
-
-  <main class="flex w-11 items-center justify-center">Lets Start</main>
-  <div class="flex h-11 w-10 items-center justify-center p-5"></div>
+  <div
+    class="flex h-full w-full flex-col items-center justify-center gap-6 tablet:gap-10 tablet:px-12 small:gap-12 small:px-14 large:gap-[4.125rem]"
+  >
+    <div v-if="width >= 1240" class="flex h-[630px] w-full flex-row gap-6">
+      <HomeHero />
+      <HomeAllNews />
+    </div>
+    <div v-else>
+      <HomeHero />
+    </div>
+    <div class="flex h-full w-full flex-col gap-6 tablet:flex-row tablet:gap-8" v-if="width < 1240">
+      <HomeAllNews />
+      <HomePopularNews />
+    </div>
+  </div>
 </template>
