bdfe613bf885246dc4ec7e40bbfb2aa900291276
olegbobokhidze <oleg.bobokhidze.1@iliauni.edu.ge>
2024-03-29 19:14:26 +0400

    wip: add famous fact config and image

---
+++ src/config/FamousFactsConfig.ts
@@ -0,0 +1,58 @@
+export type IFamousFactsConfig = {
+  id: number
+  description: string
+  image: string
+}
+
+export const FamousFactsConfig: IFamousFactsConfig[] = [
+  {
+    id: 0,
+    description: 'ქვემო ქართლის რეგიონში 20 მლნ ლარამდე ღირებულების',
+    image: './assets/images/home-famous-facts-image.png'
+  },
+  {
+    id: 1,
+    description: 'ქვემო ქართლის რეგიონში 20 მლნ ლარამდე ღირებულების',
+    image: './assets/images/home-famous-facts-image.png'
+  },
+  {
+    id: 2,
+    description: 'ქვემო ქართლის რეგიონში 20 მლნ ლარამდე ღირებულების',
+    image: './assets/images/home-famous-facts-image.png'
+  },
+  {
+    id: 3,
+    description: 'ქვემო ქართლის რეგიონში 20 მლნ ლარამდე ღირებულების',
+    image: './assets/images/home-famous-facts-image.png'
+  },
+  {
+    id: 4,
+    description: 'ქვემო ქართლის რეგიონში 20 მლნ ლარამდე ღირებულების',
+    image: './assets/images/home-famous-facts-image.png'
+  },
+  {
+    id: 5,
+    description: 'ქვემო ქართლის რეგიონში 20 მლნ ლარამდე ღირებულების',
+    image: './assets/images/home-famous-facts-image.png'
+  },
+  {
+    id: 6,
+    description: 'ქვემო ქართლის რეგიონში 20 მლნ ლარამდე ღირებულების',
+    image: './assets/images/home-famous-facts-image.png'
+  },
+  {
+    id: 7,
+    description: 'ქვემო ქართლის რეგიონში 20 მლნ ლარამდე ღირებულების',
+    image: './assets/images/home-famous-facts-image.png'
+  },
+  {
+    id: 8,
+    description: 'ქვემო ქართლის რეგიონში 20 მლნ ლარამდე ღირებულების',
+    image: './assets/images/home-famous-facts-image.png'
+  },
+  {
+    id: 8,
+    description: 'ქვემო ქართლის რეგიონში 20 მლნ ლარამდე ღირებულების',
+    image: './assets/images/home-famous-facts-image.png'
+  }
+]
