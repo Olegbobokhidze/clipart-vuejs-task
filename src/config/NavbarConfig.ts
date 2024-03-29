4bd3283e1bacdfae268c9f04406a5805db45d3f9
olegbobokhidze <oleg.bobokhidze.1@iliauni.edu.ge>
2024-03-30 02:00:08 +0400

    feat: add navbar config

---
+++ src/config/NavbarConfig.ts
@@ -0,0 +1,35 @@
+export type INavbarConfig = {
+  id: number
+  title: string
+}
+
+export const NavbarConfig: INavbarConfig[] = [
+  {
+    id: 0,
+    title: 'პოლიტიკა'
+  },
+  {
+    id: 1,
+    title: 'საზოგადოება'
+  },
+  {
+    id: 2,
+    title: 'სამართალი'
+  },
+  {
+    id: 3,
+    title: 'ბიზნესი & ეკონომიკა'
+  },
+  {
+    id: 4,
+    title: 'ბიზნესი & ეკონომიკა'
+  },
+  {
+    id: 5,
+    title: 'ეკონომიკა'
+  },
+  {
+    id: 6,
+    title: 'რელიგია'
+  }
+]
