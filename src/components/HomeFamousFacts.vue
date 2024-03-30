ab702898f92aa982debbb71fc05ab781af18c341
olegbobokhidze <oleg.bobokhidze.1@iliauni.edu.ge>
2024-03-30 13:40:50 +0400

    feat: add home famous facts and home hero sliders

--- src/components/HomeFamousFacts.vue
+++ src/components/HomeFamousFacts.vue
@@ -1,23 +1,58 @@
-<script setup lang="ts">
-import { FamousFactsConfig } from '@/config/FamousFactsConfig'
-</script>
-
 <template>
   <div class="flex h-full w-full flex-col gap-4 rounded-[1.25rem] bg-white px-4 pb-6 pt-3">
     <h1 class="text-base text-[#1D2D35]">ცნობილი ფაქტები</h1>
-    <div class="flex flex-row gap-4">
-      <div
-        class="flex h-full w-[312px] flex-col items-center justify-center gap-3 rounded-[1.25rem] bg-[#F5F8FF] px-3 pb-4 pt-3"
-      >
-        <div class="h-[7.5rem] w-full">
-          <img class="h-full w-full" src="@/assets/images/home-famous-facts-image.png" />
+    <swiper
+      :modules="modules"
+      :breakpoints="{
+        500: { slidesPerView: 1.5 },
+        768: { slidesPerView: 2.5 },
+        1024: { slidesPerView: 3 },
+        1440: { slidesPerView: 3.5 }
+      }"
+      :space-between="50"
+      navigation
+      :pagination="{ clickable: true }"
+      :scrollbar="{ draggable: true }"
+      class="w-full"
+    >
+      <swiper-slide class="w-full" v-for="item in FamousFactsConfig" :key="item.id">
+        <div class="flex flex-row gap-4">
+          <div
+            class="flex h-full w-[312px] flex-col items-center justify-center gap-3 rounded-[1.25rem] bg-[#F5F8FF] px-3 pb-4 pt-3"
+          >
+            <div class="h-[7.5rem] w-full">
+              <img class="h-full w-full" :src="item.image" />
+            </div>
+            <p class="line-clamp-5 text-[0.75rem] text-[#1D2D35]">
+              {{ item.description }}
+            </p>
+          </div>
         </div>
-        <p class="line-clamp-5 text-[0.75rem] text-[#1D2D35]">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita placeat facere mollitia
-          aspernatur neque veritatis deleniti, quae porro vel eveniet dolorum ea repellendus! Rerum
-          similique aliquam optio fuga accusamus et.
-        </p>
-      </div>
-    </div>
+      </swiper-slide>
+    </swiper>
   </div>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { Navigation, Pagination, Scrollbar } from 'swiper/modules'
+import { Swiper, SwiperSlide } from 'swiper/vue'
+
+import { FamousFactsConfig } from '@/config/FamousFactsConfig'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import 'swiper/css/scrollbar'
+
+const modules = ref([Navigation, Pagination, Scrollbar])
+</script>
+
+<style>
+.swiper-button-next,
+.swiper-button-prev {
+  display: none;
+}
+.swiper-scrollbar {
+  display: none;
+}
+</style>
--- src/components/HomeHero.vue
+++ src/components/HomeHero.vue
@@ -1,13 +1,50 @@
-<script setup lang="ts"></script>
+<script lang="ts">
+import { ref } from 'vue'
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules'
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import 'swiper/css/scrollbar'
+import { HomeHeroConfig } from '@/config/HomeHeroConfig'
+export default {
+  components: {
+    Swiper,
+    SwiperSlide
+  },
+  setup() {
+    const SliderImg = ref(null)
+
+    return {
+      SliderImg,
+      HomeHeroConfig,
+      modules: [Navigation, Pagination, Scrollbar, A11y]
+    }
+  }
+}
+</script>
 <template>
-  <div class="flex h-full w-full flex-col gap-4  rounded-[1.25rem] bg-white px-4 pb-6 pt-3">
-    <div class="h-full w-full">
-      <img
-        src="@/assets/images/home-hero-image.jpeg"
-        alt="home-hero-image"
-        class="max-h-[25.375rem] w-full rounded-[1.25rem]"
-      />
-    </div>
+  <div
+    class="flex h-full w-full flex-col gap-4 rounded-[1.25rem] bg-white px-4 pb-6 pt-3 small:max-w-[80%]"
+  >
+    <swiper
+      :modules="modules"
+      :slides-per-view="1"
+      :space-between="50"
+      navigation
+      :pagination="{ clickable: true }"
+      class="w-full"
+    >
+      <swiper-slide class="w-full" v-bind:key="item.id" v-for="item in HomeHeroConfig">
+        <div class="h-full w-full">
+          <img
+            :src="item.image"
+            alt="home-hero-image"
+            class="max-h-[25.375rem] w-full rounded-[1.25rem]"
+          />
+        </div>
+      </swiper-slide>
+    </swiper>
     <div class="flex flex-col gap-4">
       <div class="flex gap-1">
         <div
@@ -31,3 +68,51 @@
     </div>
   </div>
 </template>
+<style>
+.swiper-pagination-bullet {
+  background-color: white;
+  width: 10px;
+  height: 10px;
+}
+
+.swiper-button-next,
+.swiper-button-prev {
+  background-color: white;
+  width: 45px;
+  height: 45px;
+  border-radius: 100%;
+  color: black;
+  transform: translateY(-50%);
+  margin: 0 20px 0 20px;
+}
+
+@media only screen and (max-width: 1024px) {
+  .swiper-button-next,
+  .swiper-button-prev {
+    display: none !important;
+  }
+
+  .swiper-pagination {
+    transform: translateY(0%) !important;
+  }
+}
+
+.swiper {
+  margin: 0;
+}
+
+.swiper-button-prev:after,
+.swiper-button-next:after {
+  font-size: 16px;
+}
+
+.swiper-pagination-bullet-active {
+  background-color: white;
+  width: 30px;
+  border-radius: 4px;
+}
+
+.swiper-pagination {
+  transform: translateY(0%);
+}
+</style>
