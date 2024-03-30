dde7318da2a5705b4988520380303a46cf407390
olegbobokhidze <oleg.bobokhidze.1@iliauni.edu.ge>
2024-03-30 13:55:03 +0400

    fix: image paths from config in production

--- src/config/NewsConfig.ts
+++ src/config/NewsConfig.ts
@@ -36,56 +36,56 @@ export const NewsConfig: INewsConfig[] = [
     id: 5,
     description: 'დღეიდან ასაწონად განკუთვნილი პოლიეთილენის პარკები აიკრძალა',
     category: 'popular',
-    image: 'src/assets/images/home-news-popular-image.png',
+    image: './src/assets/images/home-news-popular-image.png',
     time: '7:30 02.01.2023'
   },
   {
     id: 6,
     description: 'დღეიდან ასაწონად განკუთვნილი პოლიეთილენის პარკები აიკრძალა',
     category: 'popular',
-    image: 'src/assets/images/home-news-popular-image.png',
+    image: './src/assets/images/home-news-popular-image.png',
     time: '7:30 02.01.2023'
   },
   {
     id: 7,
     description: 'დღეიდან ასაწონად განკუთვნილი პოლიეთილენის პარკები აიკრძალა',
     category: 'popular',
-    image: 'src/assets/images/home-news-popular-image.png',
+    image: './src/assets/images/home-news-popular-image.png',
     time: '7:30 02.01.2023'
   },
   {
     id: 8,
     description: 'დღეიდან ასაწონად განკუთვნილი პოლიეთილენის პარკები აიკრძალა',
     category: 'popular',
-    image: 'src/assets/images/home-news-popular-image.png',
+    image: './src/assets/images/home-news-popular-image.png',
     time: '7:30 02.01.2023'
   },
   {
     id: 9,
     description: 'დღეიდან ასაწონად განკუთვნილი პოლიეთილენის პარკები აიკრძალა',
     category: 'popular',
-    image: 'src/assets/images/home-news-popular-image.png',
+    image: './src/assets/images/home-news-popular-image.png',
     time: '7:30 02.01.2023'
   },
   {
     id: 10,
     description: 'დღეიდან ასაწონად განკუთვნილი პოლიეთილენის პარკები აიკრძალა',
     category: 'popular',
-    image: 'src/assets/images/home-news-popular-image.png',
+    image: './src/assets/images/home-news-popular-image.png',
     time: '7:30 02.01.2023'
   },
   {
     id: 11,
     description: 'დღეიდან ასაწონად განკუთვნილი პოლიეთილენის პარკები აიკრძალა',
     category: 'popular',
-    image: 'src/assets/images/home-news-popular-image.png',
+    image: './src/assets/images/home-news-popular-image.png',
     time: '7:30 02.01.2023'
   },
   {
     id: 12,
     description: 'დღეიდან ასაწონად განკუთვნილი პოლიეთილენის პარკები აიკრძალა',
     category: 'popular',
-    image: 'src/assets/images/home-news-popular-image.png',
+    image: './src/assets/images/home-news-popular-image.png',
     time: '7:30 02.01.2023'
   }
 ]
